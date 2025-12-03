cef76724c1771ffd79559a2bc7be1dc93f20fcff
Jacob Humphreys - Master of Silicon Lands <jahumphreys2060@gmail.com>
2025-12-02 22:24:43 -0700

    solved day 1

--- 2025/base/flake.nix
+++ 2025/base/flake.nix
@@ -1,41 +1,23 @@
 {
-  description = "Flake Dependencies for my C/CPP template for building with Zig. Generated with AI";
+  description = "devShell using flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-2505.url = "github:NixOS/nixpkgs/nixos-25.05";
-    flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell.override {
+      stdenv = pkgs.llvmPackages_latest.libcxxStdenv;
+    } {
+      packages = with pkgs;[
+        llvmPackages_latest.clang-tools
+        cmake
+        ninja
+        zig
+      ];
+
+      shellHook = ''
+      '';
+    };
   };
-
-  outputs = { self, nixpkgs, nixpkgs-2505,flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        pkgs2505 = import nixpkgs-2505 {
-          inherit system;
-        };
-        llvm = pkgs2505.llvmPackages_latest;
-      in {
-        devShells.default = pkgs.mkShell {
-            name = "clang-zig-shell";
-
-            stdenv = llvm.libcxxStdenv;
-
-            buildInputs = with pkgs;[
-                llvm.clang-tools
-                llvm.lldb
-                cmake
-                ninja
-                zig
-                pkg-config
-            ];
-
-        shellHook = ''
-        '';
-        };
-      }
-    );
 }
-
